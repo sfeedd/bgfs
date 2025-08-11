76e6a2a04c144bbe5e00ebd1d674ea72855581c0
sfeedd <qowjdgus86@naver.com>
2025-08-11 22:59:46 +0900

    admin, forms 코드 수정

--- Backend/teams/admin.py
+++ Backend/teams/admin.py
@@ -1,9 +1,34 @@
 from django.contrib import admin
-from .models import Player
+from .models import Team, Player, GalleryItem, Notice, Application, Contact
 from .forms import PlayerForm
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'logo', 'introduction', 'history')
+    search_fields = ('name', 'introduction')
 
 class PlayerAdmin(admin.ModelAdmin):
     form = PlayerForm
     list_display = ('name', 'number', 'gender', 'birth_date')
+    
+class GalleryItemAdmin(admin.ModelAdmin):
+    list_display = ('team', 'media_url', 'description', 'created_at')
+    search_fields = ('team__name', 'media_url', 'description')
+    
+class NoticeAdmin(admin.ModelAdmin):
+    list_display = ('team', 'title', 'content', 'created_at')
+    search_fields = ('team__name', 'title')
+    
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'contact_info', 'message', 'created_at')
+    search_fields = ('name', 'contact_info')
+    
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('team', 'email', 'kakao_id', 'phone')
+    search_fields = ('team__name', 'email', 'kakao_id')
 
+admin.site.register(Team, TeamAdmin)
 admin.site.register(Player, PlayerAdmin)
+admin.site.register(GalleryItem, GalleryItemAdmin)
+admin.site.register(Notice, NoticeAdmin)
+admin.site.register(Application, ApplicationAdmin)
+admin.site.register(Contact, ContactAdmin)
--- Backend/teams/forms.py
+++ Backend/teams/forms.py
@@ -1,5 +1,20 @@
 from django import forms
-from .models import Player
+from .models import Team, Player
+
+# class TeamForm(forms.ModelForm):
+#     photo_file = forms.ImageField(required=False)  # 업로드용 필드
+
+#     class Meta:
+#         model = Team
+#         fields = ['name', 'logo', 'introduction', 'history']
+
+#     def save(self, commit=True):
+#         instance = super().save(commit=False)
+#         if self.cleaned_data.get('logo'):
+#             instance.photo = self.cleaned_data['logo'].read()  # 파일을 BinaryField에 저장
+#         if commit:
+#             instance.save()
+#         return instance
 
 class PlayerForm(forms.ModelForm):
     photo_file = forms.ImageField(required=False)  # 업로드용 필드
